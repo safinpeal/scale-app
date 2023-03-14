7545ce78c65bc86b1c0230b57b5f4cf0ccae5710
Safinpeal222 <pealmhmd222@gmail.com>
2023-03-14 11:14:14 +0600

    list style added

--- src/Pages/Home/Lists/ProductList.js
+++ src/Pages/Home/Lists/ProductList.js
@@ -1,16 +1,35 @@
 import { Link } from "react-router-dom";
+import './ProductList.css';
 
 function ProductList(props){
     const server = "http://localhost:5000/images/";
     return(
-        <div className="border rounded-md p-3">
-            <img src={server+props.product.image} className="img-fluid"/>
-            <div>{props.product.name}</div>
-            <div>{props.product.category}</div>
+        
+          <div className="card h-100 trans">
+            <img src={server+props.product.image} className="card-img-top img-fluid" alt="..."/>
+            <div class="card-body">
+              <h5 className="card-title name">{props.product.name}</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
             <Link to={`/productdetails/${props.product._id}`}>
-               <button className="btn btn-primary">Show Details</button>
+              <button className="btn btn-primary">Show Details</button>
             </Link>
-        </div>
+            </div>
+          
+        
+</div>
+
+
+
+        // <div className="border rounded-md p-3">
+        //     <img src={server+props.product.image} className="img-fluid"/>
+        //     <div className="name">{props.product.name}</div>
+        //     <div className="category">{props.product.category}</div>
+        //     <Link to={`/productdetails/${props.product._id}`}>
+        //        <button className="btn btn-primary">Show Details</button>
+        //     </Link>
+        // </div>
     )
 }
 
